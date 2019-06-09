1c952c34df23d74e825522b7776ed85add36f660
Nathalia Eskelsen <nathalia.eskelsen@hotmail.com>
2019-06-09 17:10:38 -0300

    Update faceRecognition.py

--- faceRecognition.py
+++ faceRecognition.py
@@ -6,7 +6,9 @@ assert subscription_key
 
 face_api_url = 'https://westeurope.api.cognitive.microsoft.com/face/v1.0/detect'
 
-image_url = 'https://media.licdn.com/dms/image/C4D03AQFRZU2NnHGICw/profile-displayphoto-shrink_200_200/0?e=1565222400&v=beta&t=ScEBVnvOpx0pe8yFPj3gwTlB97vgJ5RSqcs6NWYGwTE'
+#newImage = input("Please input image URL: ")
+
+image_url = "https://media.licdn.com/dms/image/C4D03AQFRZU2NnHGICw/profile-displayphoto-shrink_200_200/0?e=1565222400&v=beta&t=ScEBVnvOpx0pe8yFPj3gwTlB97vgJ5RSqcs6NWYGwTE"
 
 headers = {'Ocp-Apim-Subscription-Key': subscription_key}
 
@@ -17,4 +19,11 @@ params = {
 }
 
 response = requests.post(face_api_url, params=params, headers=headers, json={"url": image_url})
-print(json.dumps(response.json()))
+print("Detection result:")
+#print(json.dumps(response.json()))
+text = json.dumps(response.json())
+
+allWords = text.split(",")
+
+for i in allWords:
+    print(i)
